0918dea5b910fdffd2228876768020e5d1c0be03
Mithos38 <lucas.chessa@laposte.net>
2014-06-03 09:50:34 +0200

    MAJ ControleurImpl.java

--- projet/tiers2/ControleurImpl.java
+++ projet/tiers2/ControleurImpl.java
@@ -8,24 +8,24 @@ import projet.tiers3.Modele;
 import projet.tiers3.Utilisateur;
 
 public class ControleurImpl {
+
+	// Attibuts
+	private ArrayList<Tweet> listeTweets;
+	private ArrayList<Utilisateur> listeUtil;
 	
+	// Constructeurs
 	public ControleurImpl() {
-		this.listeTweets = new ArrayList();
+		this.listeTweets = new ArrayList<Tweet>();
+		this.listeUtil = new ArrayList<Utilisateur>();
 	}
 
-	public ControleurImpl(ArrayList<Tweet> listeTweets) {
+	public ControleurImpl(ArrayList<Tweet> listeTweets, ArrayList<Utilisateur> listeUtil) {
 		super();
 		this.listeTweets = listeTweets;
+		this.listeUtil = listeUtil;
 	}
-
-	private ArrayList<Tweet> listeTweets;
 	
-	public void recupListeTweets(Modele modele) throws RemoteException {
-		ArrayList<Utilisateur> listeInscrits = modele.getListeInscrits();
-		for (int i=0;i<listeInscrits.size();i++) {
-			this.listeTweets.addAll(listeInscrits.get(i).getListeTweets());
-		}
-	}
+	// Accesseurs & Moidifieurs
 
 	public ArrayList<Tweet> getListeTweets() {
 		return listeTweets;
@@ -34,5 +34,46 @@ public class ControleurImpl {
 	public void setListeTweets(ArrayList<Tweet> listeTweets) {
 		this.listeTweets = listeTweets;
 	}
+
+	public ArrayList<Utilisateur> getListeUtil() {
+		return listeUtil;
+	}
+
+	public void setListeUtil(ArrayList<Utilisateur> listeUtil) {
+		this.listeUtil = listeUtil;
+	}
 	
+	// Methodes
+	public void recupListeTweets(Modele modele) throws RemoteException {
+		ArrayList<Utilisateur> listeInscrits = modele.getListeInscrits();
+		for (int i=0;i<listeInscrits.size();i++) {
+			this.listeTweets.addAll(listeInscrits.get(i).getListeTweets());
+		}
+	}
+	
+	public void recupListeUtil(Modele modele) throws RemoteException {
+		this.listeUtil = modele.getListeInscrits();
+	}
+	
+	public boolean connexion(String idUtil, String mdpUtil){
+		boolean connexion = false;
+		Utilisateur Util = getUtilByID(idUtil);
+		if (Util != null){
+			if (Util.getMDPUtilisateur() == mdpUtil){
+				connexion = true;
+			}
+		}
+		return connexion;
+	}
+	
+	public Utilisateur getUtilByID(String idUtil){
+		Utilisateur res = null;
+		for (int indice = 0; indice < listeUtil.size(); indice++){
+			if (listeUtil.get(indice).getIdUtilisateur()==idUtil){
+				res = listeUtil.get(indice);
+				break;
+			}
+		}
+		return res;
+	}
 }
